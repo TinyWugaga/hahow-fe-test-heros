80289980327d110f4566889fd1b864f08c7a38de
Tinywugaga <tiny98712@gmail.com>
2023-02-24 14:38:32 +0800

    add libraries and set configs

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,25 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        {/* metadata */}
+        <meta property="og:title" content="Hahow Heros!" key="title" />
+        <meta
+          property="og:description"
+          content="Hahow FE Test Project"
+          key="description"
+        />
+
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
