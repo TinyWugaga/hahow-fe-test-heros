9a0958dfa64cf12ece864256fe2ce7b8b97e3836
Tinywugaga <tiny98712@gmail.com>
2023-02-25 16:52:47 +0800

    add fetchHero API in HerosPage

--- src/pages/heros/index.tsx
+++ src/pages/heros/index.tsx
@@ -1,12 +1,39 @@
 import { useState, useEffect } from "react";
 
+interface Hero {
+  id: string;
+  name: string;
+  image: string;
+}
+
 export default function HerosPage() {
-  const [heros, seHeros] = useState([]);
+  const [heros, seHeros] = useState<Hero[]>([]);
 
   useEffect(() => {
-    fetch("https://hahow-recruit.herokuapp.com/heroes").then((res) =>
-      console.log({ res })
-    );
-  });
-  return <div></div>;
+    const fetchHeros = async () => {
+      try {
+        const response = await fetch(
+          "https://hahow-recruit.herokuapp.com/heroes",
+          {
+            headers: {
+              Accept: "application/json",
+              "Content-Type": "application/json",
+            },
+          }
+        );
+        const hahowHeros = await response.json();
+        seHeros(hahowHeros);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchHeros();
+  }, []);
+  return (
+    <div>
+      {heros.map((hero) => (
+        <p key={hero.id}>{hero.name}</p>
+      ))}
+    </div>
+  );
 }
